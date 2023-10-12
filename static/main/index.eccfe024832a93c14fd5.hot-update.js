a146148e4abfd01081046367c64183c29488513c
Justin Luu <justinluu@Justins-MBP.attlocal.net>
2023-10-11 21:42:44 -0700

    static

---
+++ static/main/index.eccfe024832a93c14fd5.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatedjango_react_template"]("main/index",{
+
+/***/ "./frontend/main/StyleCalculator.tsx":
+/*!*******************************************!*\
+  !*** ./frontend/main/StyleCalculator.tsx ***!
+  \*******************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (25:15)\\nFile was processed with these loaders:\\n * ./node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n|     const { isLoading, error, data } = useQuery('style', fetchItems);\\n|     const [selectedFabric, setSelectedFabric] = useState('');\\n>     useEffect(() = {}, console.log('data', data));\\n|     return (React.createElement(Stack, { alignItems: \\\"center\\\", useFlexGap: true, gap: \\\"40px\\\" },\\n|         React.createElement(Select, { value: 10 }, data && data['fabric_types'].map((fabricType) => { return React.createElement(MenuItem, { value: fabricType.id }, fabricType.label); }))));\");\n\n//# sourceURL=webpack://django-react-template/./frontend/main/StyleCalculator.tsx?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("8115239a44db4b35fa7d")
+/******/ })();
+/******/ 
+/******/ }
+);
