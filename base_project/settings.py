8a3fd13e4bc8dc2cccc807ddc49907baaeb2ee4f
Justin Luu <justinluu@Justins-MBP.attlocal.net>
2023-12-16 19:42:31 -0800

    remove from allowed host

--- base_project/settings.py
+++ base_project/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -65,12 +65,13 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 
 ]
-CORS_ALLOW_HEADERS = ['*']
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'http://clothing-calculator-env.eba-qnfpfgsz.us-west-2.elasticbeanstalk.com',
     'https://clothing-calculator-env.eba-qnfpfgsz.us-west-2.elasticbeanstalk.com',
+    '*',
 
 ]
 CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
